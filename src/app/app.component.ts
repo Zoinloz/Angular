c951f69c8b863e7605352e91cc16aab5b7c5eeb5
Zoinloz <blanckevin19@gmail.com>
2019-11-18 12:14:29 +0100

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-TP';
+  title = 'Angular';
+  clickme(){
+    this.title = 'Angular-TP';
+  }
 }
