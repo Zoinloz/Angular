f1c8b211e93731d0a6b2f8d439bf4b3636ba97ea
Zoinloz <blanckevin19@gmail.com>
2019-12-02 10:30:02 +0100

    Class interface + login api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ErrorHandler } from '@angular/core';
 import { EventHandlerVars } from '@angular/compiler/src/compiler_util/expression_converter';
 import { timeout } from 'q';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import IUser from '../models/IUser';
 
 @Component({
   selector: 'app-login',
@@ -20,39 +21,71 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
 
-    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(value => {
-      console.log(value);
-    });
-    console.log('ici');
 
   }
 
   connexion() {
+
+    // Connexion que avec un login 
+    this.http.get('https://jsonplaceholder.typicode.com/users?username='+this.login).subscribe((value: IUser[]) => {
+      console.log('get : '+ this.login ,value.length);
+
+       
+        
+        setTimeout(() => {
+
+          if(value.length === 1){
+            this.logged = true; 
+          
+             this._snackBar.open("Bonjour, "+this.login+" !", null, {
+               duration: 2000,
+
+             });
+           }
+    
+           else {
+             this._snackBar.open("Wrong Logged", null, {
+               duration: 2000,
+             });
+           }
+    
+          this.loading = false;
+    
+        }, 2000);
+
+      
+
+    
+    }, (error: HttpErrorResponse)=>{
+      console.error('error', error);
+    });
+
     console.log('login', this.login);
     console.log('mdp', this.mdp);
 
     this.loading = true;
 
-    setTimeout(() => {
+    // Connexion avec un MDP
+    // setTimeout(() => {
 
-      if (this.login === 'admin' && this.mdp === 'admin') {
-        this.logged = true;
-        this._snackBar.open("Logged", null, {
-          duration: 2000,
-        });
-      }
+    //   if (this.login === 'admin' && this.mdp === 'admin') {
+    //     this.logged = true;
+    //     this._snackBar.open("Logged", null, {
+    //       duration: 2000,
+    //     });
+    //   }
 
-      else {
-        this._snackBar.open("Wrong Logged", null, {
-          duration: 2000,
-        });
-      }
+    //   else {
+    //     this._snackBar.open("Wrong Logged", null, {
+    //       duration: 2000,
+    //     });
+    //   }
 
-      this.loading = false;
+    //   this.loading = false;
 
-    }, 2000);
+    // }, 2000);
 
-
+ 
 
   }
 
