70ee6585062688a2f8ca98b645f96c6d24222de2
Zoinloz <blanckevin19@gmail.com>
2019-12-09 09:18:51 +0100

    hebergement git + fichier build

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { MatSpinner, MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +12,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports :[FormsModule, MatProgressSpinnerModule, MatInputModule, HttpClientModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -22,4 +27,19 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('good init', () => {
+    
+    expect(component.logged).toBeTruthy();
+    expect(component.login).toBeTruthy();
+    
+  });
+
+  it('connexion', () => {
+    
+    expect(component.logged).toBeTruthy();
+    component.connexion();
+    expect(component.connexion()).toBeTruthy();
+  });
+
 });
